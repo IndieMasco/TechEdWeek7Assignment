d0b694a7cf9bd28c42a0231ba94c4f9230577636
sam <s.clarkyy.1994@gmail.com>
2025-09-26 22:37:11 +0100

    back track

--- server/server.js
+++ server/server.js
@@ -1,86 +1,20 @@
-// // Basic set up
-// import express from "express";
-// import cors from "cors";
-// import { db } from "./dbConnection.js";
-
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-
-// const PORT = 8080;
-// app.listen(PORT, function () {
-//   console.info(` Server is running in port ${PORT}`);
-// });
-
-// app.get("/", (_, res) => {
-//   res.send("Welcome");
-// });
-
-// // Read data from my games table
-// app.get("/games", async (_, res) => {
-//   try {
-//     const data = await db.query(`SELECT id, name FROM games;`);
-//     res.json(data.rows);
-//   } catch (error) {
-//     console.error("Error in games route!", error);
-//     res.status(500).json({ success: false });
-//   }
-// });
-
-// // Read data from my reviews table
-// app.get("/review", async (_, res) => {
-//   try {
-//     const data = await db.query(`SELECT name, review FROM review;`);
-//     res.json(data.rows);
-//   } catch (error) {
-//     console.error("Error in review route!", error);
-//     res.status(500).json({ success: false });
-//   }
-// });
-
-// // Read data from the games and review
-// app.get("/games-reviews", async (_, res) => {
-//   try {
-//     const data = await db.query(`
-//     SELECT games.name AS "Game name", review.name AS "Users name", review.review AS "User review"
-//     FROM review JOIN games ON games.id = review.games_id;`);
-//     res.json(data.rows);
-//   } catch (error) {
-//     console.error("Error in the game-reviews route!", error);
-//     res.status(500).json({ success: false });
-//   }
-// });
-
-// // Create new data in the reviews table
-// app.post("/add-reviews", (req, res) => {
-//   const { name, review, gamesid } = req.body;
-//   try {
-//     const query = db.query(
-//       `INSERT INTO review (name, review, games_id) VALUES ($1, $2, $3);`,
-//       [name, review, gamesid]
-//     );
-//     res.status(200).json({ success: true });
-//   } catch (error) {
-//     console.error("Error in add-reviews route", error);
-//     res.status(500).json({ success: false });
-//   }
-// });
-
-//=====================================
-
+// Basic set up
 import express from "express";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 import { db } from "./dbConnection.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const PORT = 8080;
+app.listen(PORT, function () {
+  console.info(` Server is running in port ${PORT}`);
+});
+
+app.get("/", (_, res) => {
+  res.send("Welcome");
+});
 
 // Read data from my games table
 app.get("/games", async (_, res) => {
@@ -108,8 +42,8 @@ app.get("/review", async (_, res) => {
 app.get("/games-reviews", async (_, res) => {
   try {
     const data = await db.query(`
-      SELECT games.name AS "Game name", review.name AS "Users name", review.review AS "User review"
-      FROM review JOIN games ON games.id = review.games_id;`);
+    SELECT games.name AS "Game name", review.name AS "Users name", review.review AS "User review"
+    FROM review JOIN games ON games.id = review.games_id;`);
     res.json(data.rows);
   } catch (error) {
     console.error("Error in the game-reviews route!", error);
@@ -121,7 +55,7 @@ app.get("/games-reviews", async (_, res) => {
 app.post("/add-reviews", (req, res) => {
   const { name, review, gamesid } = req.body;
   try {
-    db.query(
+    const query = db.query(
       `INSERT INTO review (name, review, games_id) VALUES ($1, $2, $3);`,
       [name, review, gamesid]
     );
@@ -130,17 +64,4 @@ app.post("/add-reviews", (req, res) => {
     console.error("Error in add-reviews route", error);
     res.status(500).json({ success: false });
   }
-});
-
-// --- THE REACT ROUTER FIX ---
-
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-const PORT = 8080;
-app.listen(PORT, function () {
-  console.info(` Server is running in port ${PORT}`);
 });
