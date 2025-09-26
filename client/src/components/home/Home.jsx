dbdb82d3510b0b9dbc3b9d782ee259b099cc8436
sam <s.clarkyy.1994@gmail.com>
2025-09-26 22:14:31 +0100

    styling is done apart from the FOOTER

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,30 @@
 // import Footer from "../footer/Footer";
-
+import HomePage from "./HomePage.png";
 import "./Home.css";
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome</h1>
-      <p>This will be for an about section</p>
+      <div className="home-page-wrapper">
+        <div className="home-container">
+          <h1 className="home-header">Welcome to The Game Loop</h1>
+          <img
+            className="home-image"
+            src={HomePage}
+            alt="The main image for the home page"
+          />
+          <p className="home-text">
+            Stop guessing and start knowing! This is where the gaming community
+            comes together to share their true opinions on all things
+            interactive. Whether you're hunting for your next obsession or
+            warning others about a dud, your voice matters. Read deep-dive
+            reviews, browse player scores, and join the conversation to help
+            every gamer make the best purchase decisions. Your next great game
+            is just a review away!
+          </p>
+        </div>
+      </div>
+      {/* <Footer /> */}
     </>
   );
 }
--- client/src/components/notfound/NotFound.jsx
+++ client/src/components/notfound/NotFound.jsx
@@ -1,11 +1,24 @@
 import { Link } from "react-router";
+import "./NotFound.css";
 
 export default function NotFound() {
   return (
-    <>
-      <h1>404 Not Found</h1>
-      <p>You seem to be lost.</p>
-      <Link to={"/"}>Go Home</Link>
-    </>
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h2 className="not-found-heading">404 Not Found</h2>
+        <p className="not-found-text">
+          Even John Travolta is here looking around, wondering where we put that
+          page.
+        </p>
+        <img
+          className="not-found-gif"
+          src="https://media1.tenor.com/m/fuiG0_pco2wAAAAd/confused-john-travolta.gif"
+          alt="Confused John Travolta GIF"
+        />
+        <Link to={"/"} className="not-found-link">
+          Go Home
+        </Link>
+      </div>
+    </div>
   );
 }
