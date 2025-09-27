c9ac2576c519fdc6177f0263a7264bfebdbfc38b
sam <s.clarkyy.1994@gmail.com>
2025-09-27 23:00:26 +0100

    edited the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 export default function Form() {
   return (
     <div className="footer-container">
-      <p className="text1">PLEASE</p>
-      <p className="text2">Hopefully this wont be a struggle this assignment</p>
+      <p className="text1">Made by S P J Clark</p>
+      <p className="text2">
+        {" "}
+        Quote of the day: The strongest people are not those who show strength
+        in front of us but those who win battles we know nothing about
+      </p>
     </div>
   );
 }
