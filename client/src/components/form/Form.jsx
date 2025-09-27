71f181aa34b6110b53362ea7133885c01c002abc
sam <s.clarkyy.1994@gmail.com>
2025-09-27 15:10:53 +0100

    testing

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -15,7 +15,7 @@ export default function Form() {
     async function fetchGames() {
       try {
         const response = await fetch(
-          `${import.meta.env.VITE_RENDER_KEY}/games`
+          import.meta.env.VITE_RENDER_KEY + `/games`
         );
         if (response.ok) {
           const data = await response.json();
@@ -47,7 +47,7 @@ export default function Form() {
     // Handles POST request to server
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_RENDER_KEY}/add-reviews`,
+        import.meta.env.VITE_RENDER_KEY + `/add-reviews`,
         {
           method: "POST",
           headers: {
